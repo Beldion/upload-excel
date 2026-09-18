cd0ea52949ecfa9855fde57775100aab48dce3c1
Beldion <bgbalanan@gmail.com>
2026-09-18 21:00:30 +0800

    Update page.tsx

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -3,21 +3,38 @@
 import Script from "next/script";
 
 export default function ARPage() {
+  const scene = `
+    <a-scene
+      vr-mode-ui="enabled: false"
+      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
+      renderer="antialias: true; alpha: true"
+      embedded
+    >
+      <a-camera
+        gps-new-camera="gpsMinDistance: 5"
+      ></a-camera>
+
+      <a-entity
+        material="color: red"
+        geometry="primitive: box"
+        gps-new-entity-place="latitude: 14.6507; longitude: 121.1029"
+        scale="10 10 10"
+      ></a-entity>
+    </a-scene>
+  `;
+
   return (
     <>
-      {/* A-Frame */}
       <Script
         src="https://aframe.io/releases/1.6.0/aframe.min.js"
-        strategy="beforeInteractive"
+        strategy="afterInteractive"
       />
 
-      {/* AR.js Location */}
       <Script
         src="https://raw.githack.com/AR-js-org/AR.js/3.4.7/three.js/build/ar-threex-location-only.js"
         strategy="afterInteractive"
       />
 
-      {/* AR.js A-Frame */}
       <Script
         src="https://raw.githack.com/AR-js-org/AR.js/3.4.7/aframe/build/aframe-ar.js"
         strategy="afterInteractive"
@@ -25,37 +42,16 @@ export default function ARPage() {
 
       <div
         style={{
+          position: "fixed",
+          inset: 0,
           width: "100vw",
           height: "100vh",
           overflow: "hidden",
         }}
-      >
-        <a-scene
-          vr-mode-ui="enabled: false"
-          arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false"
-          renderer="antialias: true; alpha: true"
-          embedded
-        >
-          <a-camera gps-new-camera="gpsMinDistance: 5"></a-camera>
-
-          <a-entity
-            material="color: red"
-            geometry="primitive: box"
-            gps-new-entity-place="latitude: 14.6507; longitude: 121.1029"
-            scale="10 10 10"
-          ></a-entity>
-        </a-scene>
-      </div>
+        dangerouslySetInnerHTML={{
+          __html: scene,
+        }}
+      />
     </>
   );
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "a-scene": any;
-      "a-camera": any;
-      "a-entity": any;
-    }
-  }
 }
